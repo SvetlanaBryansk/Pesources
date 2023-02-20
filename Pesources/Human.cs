d061d6d1ad98bbdac49eb02c5a79e016a42c2387
Светлана <123038012+SvetlanaBryansk@users.noreply.github.com>
2023-02-20 03:04:41 +0300

    Add files via upload

---
+++ Pesources/Human.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+
+
+namespace Pesources
+{
+    public class Human: IDisposable
+    {
+        private string _name;
+        private bool disposedValue;
+
+        public Human(string name)
+        {
+            _name = name;
+        }
+
+        private void ReleaseUnmanagedResources()
+        
+            
+                {
+                    // TODO: освободить управляемое состояние (управляемые объекты)
+                }
+
+              
+        public void Dispose()
+        {
+           ReleaseUnmanagedResources();
+            GC.SuppressFinalize(this);
+        }
+        ~Human()
+        {
+            ReleaseUnmanagedResources ();
+        }
+    }
+}
---
+++ Pesources/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace Pesources
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //Human human = null;
+
+            //try
+            //{
+            //    human = new Human(name: "John");
+            //}
+            //finally
+            //{
+            //    human?.Dispose();// это тоже самоу, что и это: if (human!= null)  human.Dispose(); 
+            //}
+                 
+            using (Human human = new Human (name:"John"))
+            {
+                // работа с ресурсом
+            }
+            Console.WriteLine("Какой-то код");
+
+        }
+
+        private static void GreateHumanTokill(string name)
+        {
+            Human human = new Human(name);
+        }
+    }
+}
